fa5992d9f22cff3a124a056ea7fb0b08f93a48eb
Cory Grant <cory.grant11@gmail.com>
2025-12-16 00:40:16 -0500

    caninputs switch to card and dialog

--- contracts/Devices/Pdm/Functions/CanInputDto.cs
+++ contracts/Devices/Pdm/Functions/CanInputDto.cs
@@ -3,10 +3,10 @@ using domain.Enums;
 
 namespace contracts.Devices.Pdm.Functions;
 
-public class CanInputDto
+public class CanInputDto(int number)
 {
     // Config properties
-    public int Number { get; set; }
+    public int Number { get; } = number;
     public bool Enabled { get; set; }
     public string Name { get; set; } = string.Empty;
     public bool TimeoutEnabled { get; set; }
@@ -22,4 +22,36 @@ public class CanInputDto
     // State properties
     public bool Output { get; set; }
     public int Value { get; set; }
+    
+    public CanInputDto(CanInputDto source) : this(source.Number)
+    {
+        Enabled = source.Enabled;
+        Name = source.Name;
+        TimeoutEnabled = source.TimeoutEnabled;
+        Timeout = source.Timeout;
+        Ide = source.Ide;
+        StartingByte = source.StartingByte;
+        Dlc = source.Dlc;
+        Operator = source.Operator;
+        OnVal = source.OnVal;
+        Mode = source.Mode;
+        Id = source.Id;
+        Output = source.Output;
+        Value = source.Value;
+    }
+    
+    public void CopyFrom(CanInputDto source)
+    {
+        Enabled = source.Enabled;
+        Name = source.Name;
+        TimeoutEnabled = source.TimeoutEnabled;
+        Timeout = source.Timeout;
+        Ide = source.Ide;
+        StartingByte = source.StartingByte;
+        Dlc = source.Dlc;
+        Operator = source.Operator;
+        OnVal = source.OnVal;
+        Mode = source.Mode;
+        Id = source.Id;
+    }
 }
